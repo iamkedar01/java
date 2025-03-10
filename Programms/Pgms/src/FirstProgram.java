71355fd364719cf4ddcbc255de5113a67d2db78d
Kedar Dhage <kedardhage0@gmail.com>
2025-03-10 22:22:33 +0530

    programms

---
+++ Programms/Pgms/src/FirstProgram.java
@@ -0,0 +1,10 @@
+class FirstProgram {
+    public static void main(String[] args) {
+        System.out.print("Welcome ");// use the both 1&Multi comments
+        /*  if you remove the "ln" from the "println"
+        After the word it not move the next line print the output in single line  */
+        System.out.print(" to"); // it will not move the next line
+        System.out.println(" java Programming ");
+
+    }
+}
