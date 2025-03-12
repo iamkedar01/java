86deb80b0548d60242b93210157a16281f1a3a2c
Kedar Dhage <kedardhage0@gmail.com>
2025-03-12 23:07:26 +0530

    1Escape.java

---
+++ Leacture_code/1Escape.java
@@ -0,0 +1,8 @@
+class Escape{
+    public static void main(String[] args) {
+                System.out.println("Hello\bKGCoding..."); 
+                /*we observe the output Hello there "o" missing and overwritting the "K" on the "o"
+                this escape sequence doesn't delete the string it will overwritten 
+                */
+            }
+        }
