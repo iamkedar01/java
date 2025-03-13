a634ad0a9e08020fcc196c082ba8eb68cc4b0991
Kedar Dhage <kedardhage0@gmail.com>
2025-03-13 22:45:20 +0530

    Variables

---
+++ Leacture_code/4Variables.java
@@ -0,0 +1,35 @@
+ class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+        int myNUMBER = 865;
+        int yourNumber = 0;
+
+        int asdf987 = 8;
+
+        int uh87d87yyd87 = 90; // this example will show the right variable name will easy to undesatand the code
+
+        System.out.println(yourNumber); // like this code 
+        yourNumber = 865;
+        System.out.println(yourNumber);
+        yourNumber = 45;
+        System.out.println(yourNumber + "--->This arre the Interger value");
+
+        float myFloat = 5.0f; 
+        double myDouble = 6.98766635388888888888888888548484378846;
+        System.out.println(myFloat+" -->this float value ");
+        System.out.println(myDouble+" --->This is a Double value"); 
+
+
+        boolean isVegetarian = true;  //  Assigning the boolean value 
+        System.out.println(isVegetarian+"---->using the boolean value to print");
+
+        String wishes = "Good Morning"; // Here using the string 
+        System.out.println(wishes +"--->Here use  the string value");
+
+        char myCharacter = 'K'; // Here using the character 
+        System.out.println(myCharacter+"--->Here use the character value ");
+
+
+
+    }
+}
