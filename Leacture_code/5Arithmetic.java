ce216570007be79dedcec0c8024420b040482111
Kedar Dhage <kedardhage0@gmail.com>
2025-03-14 23:40:14 +0530

    Arithmetic opertion

---
+++ Leacture_code/5Arithmetic.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Arithmetic {
+    public static void main(String[] args) {
+        System.out.println("\nWelcome to the Arithmetic opertion\n");
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the First Number: ");
+        int FirstNum = input.nextInt();
+        System.out.print("Enter the Second Number: ");
+        int SecondNum = input.nextInt();
+
+        System.out.println(FirstNum + SecondNum);  // Add the to number  give the sum 
+        System.out.println(FirstNum-SecondNum); // Subtract the two number give the sub
+        System.out.println(FirstNum*SecondNum); // give the multiplication of two number
+        System.out.println(FirstNum/SecondNum); // it give the divider
+        System.out.println(FirstNum%SecondNum); // it gives the reminder
+    }
+}
