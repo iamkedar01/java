4dc7b1622d13f730656609369a61888c2cc01291
Kedar Dhage <kedardhage0@gmail.com>
2025-03-14 23:49:33 +0530

    Assigment Opertion

---
+++ Leacture_code/6Assignment.java
@@ -0,0 +1,16 @@
+class Assignment {
+    public static void main(String[] args) {
+
+        // The Assignment oprator  symbol is "=" equal to 
+        int Myint = 7;  // Here 7 is assigning to the Myint 
+        System.out.println(Myint);
+
+        char Letter = 'K';  // Here "K" is assign to the Letter Character should be in the Single qoute
+        System.out.println(Letter);
+
+        String Name = "Kedar"; // Here "Kedar" Assign to the Name variable
+        System.out.println(Name);
+
+    }
+    
+}
