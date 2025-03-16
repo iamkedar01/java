a4eac07b147c8ed93e3e40ce92b08b6ca56ae8d5
Kedar Dhage <kedardhage0@gmail.com>
2025-03-16 22:25:59 +0530

    Order_of_Opertion

---
+++ Leacture_code/9Order_of_Opertion.java
@@ -0,0 +1,21 @@
+
+ class Order_of_Opertion {
+    public static void main(String[] args) {
+
+        System.out.println(8-3*3);// In this line first execute the mul. then sub 
+
+        System.out.println(9/(3/3 + 2));// In this line first enter the bracket inside that division then addition
+        // then after the bracket division
+
+        /* Here the some notes like how order od opertion works in code 
+         * precedence of the order of the opertion 
+         * This will work on the Badmas formula
+         * 1) First it will check the " Bracket ()  "
+         * 2) second it will check the " order like root equations"
+         * 3) third it will check the " Division or  Multifilication"
+         * 4) fourth it will check the " Addition "
+         * 5) fiveth it will check the " Substraction "
+         * 
+         */
+    }
+}
