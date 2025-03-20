98ae4bdf1d0944bb5b2b183e509330bf7bba8528
Kedar Dhage <kedardhage0@gmail.com>
2025-03-20 20:49:07 +0530

    Function in JAVA

--- Leacture_code/15Function.java
+++ Leacture_code/15Function.java
@@ -1,4 +1,23 @@
 class Function {
+    /* 
+         The basic rule of the funcation are 
+           1)---> The function should be outside the main method but inside the class.
+         2)-----> To Execute the function code you must call in the main method.
+         3)-----> 
+    */
+    public static void printFirstPattern() {
+        int rows = 0;  // initilaizeing the rows. this for how many lines of * to print 
+        while (rows < 5) {
+            System.out.print("*");
+            int i = 0; // this i will help to print each line one how line incrase * will also incrase and print 
+            while (i < rows) {  
+                System.out.print(" *");
+                i++;
+            }
+            System.out.println();
+            rows++;   // here incementing the rows till the 5 (form 0 to 5 )
+        }
+    }
     public static void main(String[] args) {
 //        System.out.println("In main method");
 //        greetUser();
@@ -28,19 +47,6 @@ class Function {
         System.out.println("*");
     }
 
-    public static void printFirstPattern() {
-        int rows = 0;  // initilaizeing the rows. this for how many lines of * to print 
-        while (rows < 5) {
-            System.out.print("*");
-            int i = 0; // this i will help to print each line one how line incrase * will also incrase and print 
-            while (i < rows) {  
-                System.out.print(" *");
-                i++;
-            }
-            System.out.println();
-            rows++;   // here incementing the rows till the 5 (form 0 to 5 )
-        }
-    }
 
     public static void greetUser() {
         System.out.println("Welcomr to the Function In JAVA............!");
