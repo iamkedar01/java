0f6fe737115d01fd41da9b48cf092b3a51b23532
Kedar Dhage <kedardhage0@gmail.com>
2025-04-14 00:26:17 +0530

    welcomeing

---
+++ JAVA_DSA/src/Class1/Welcome.java
@@ -0,0 +1,12 @@
+package Class1;
+import java.sql.SQLOutput;
+import java.util.*;
+public class Welcome {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String Name=input.next();
+        System.out.println("welcome the java programming " +Name);
+
+    }
+}
