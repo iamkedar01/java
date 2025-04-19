c5eb1a88cd0cbe795b9a4af1da4553fb853c9760
Kedar Dhage <kedardhage0@gmail.com>
2025-04-19 23:34:43 +0530

    Fibonacci Series

---
+++ JAVA_DSA/src/Class4/FibonacciSeries.java
@@ -0,0 +1,35 @@
+package Class4;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in );
+        System.out.print("Enter a number:");
+        int num = in.nextInt();
+      //  int Fseris = FibonacciSeries(num);
+        System.out.println("The Fibonacci Series number is: ");
+        FibonacciSeries(num);
+    }
+
+    // First understand the what they Asked in the questions
+    // To find the solutions first know what the main requirements of the programm
+    // Understand the pattren
+    public static void  FibonacciSeries(int num){
+        if (num< 0) return ; // check the number is less then the zero then retun nothing
+        System.out.print("0 ");
+        if (num ==0) return ; // it will print 0 if number is equal to the 0
+        System.out.print("1 ");
+        int First= 0, Second=1;  // this are first two numbers
+        while(First + Second <num){  // this condicition to to get the until the given number
+            int Third = First + Second;
+            System.out.print(Third + " "); // it used to for the iteration of the number until the contion fails
+            First = Second;
+            Second = Third;
+            // here above swaping will perfrom to change last two number evry time
+        }
+
+
+
+    }
+}
