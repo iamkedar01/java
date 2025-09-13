96949963f6d91d89b8ab5b2da00db4c0d752f07e
Kedar Dhage <kedardhage0@gmail.com>
2025-09-13 14:55:20 +0530

    Adding the file details

--- OopsConcepts/src/Main.java
+++ OopsConcepts/src/Main.java
@@ -3,6 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to the oops concept in java");
+        // Details about the which files and which concept need to learn step wise
+        /*
+            1) Class and object
+            2) This keyword
+            3)  Constructor
+            4) ComplieTimePolymorphism
+            5) Runtime Polymorphism
+         */
 
     }
 }
