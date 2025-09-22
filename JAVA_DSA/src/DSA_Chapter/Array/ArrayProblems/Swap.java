914c245cba5c93a72712c37fea4ff2cf87623b32
Kedar Dhage <kedardhage0@gmail.com>
2025-09-22 17:57:35 +0530

    Swap a Number inside the Array

---
+++ JAVA_DSA/src/DSA_Chapter/Array/ArrayProblems/Swap.java
@@ -0,0 +1,20 @@
+package DSA_Chapter.Array.ArrayProblems;
+
+import DSA_Chapter.Array.Arrays_tostring;
+
+import java.util.Arrays;
+
+public class Swap {
+    public static void main(String[] args) {
+        int[] arr = {12,2,3,56,37,23,7,9};
+        swap(arr,1,5 );
+        System.out.println(Arrays.toString(arr));
+    }
+    static void swap(int[] arr, int index1 , int index2){ // here must check the parameter type how many parameter
+        int temp = arr[index1];
+        arr[index1] = arr[index2];
+        arr[index2] = temp;
+        // here also check the where we need to change the value like inside array so check the code is properly return
+        // like arr[index] mean we changeing the inside the value in the array
+    }
+}
