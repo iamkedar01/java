c46c35581d031320834db4043308845da283e973
Kedar Dhage <kedardhage0@gmail.com>
2025-09-25 18:13:54 +0530

    Java Collection

---
+++ JavaCollections/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        // here we maintain the file details about the java collection Framework
+        System.out.println("Java Collection Framework...!");
+    }
+}
