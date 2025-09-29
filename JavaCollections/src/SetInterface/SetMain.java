b18b6f726dc16e25511a20e15267ce702c25e89d
Kedar Dhage <kedardhage0@gmail.com>
2025-09-29 21:48:33 +0530

    Set Interface and Implemented Class

---
+++ JavaCollections/src/SetInterface/SetMain.java
@@ -0,0 +1,8 @@
+package SetInterface;
+
+public class SetMain {
+    public static void main(String[] args) {
+       //  in this package has the details about the file details
+        System.out.println("Welcome to the set ");
+    }
+}
