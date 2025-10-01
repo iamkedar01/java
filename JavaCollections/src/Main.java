1e73faed1cb5cbaaf878623db78fddca9673ab88
Kedar Dhage <kedardhage0@gmail.com>
2025-10-01 11:31:06 +0530

    File Structure in Details

--- JavaCollections/src/Main.java
+++ JavaCollections/src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         // here we maintain the file details about the java collection Framework
         System.out.println("Java Collection Framework...!");
+        /*  The Order of the file Details
+                1) List Interface
+                2) Set Interface
+                3) Map Interface
+         */
     }
 }
