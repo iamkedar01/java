ff99f823f1d0bcf1dea57f07f38e62df86f7ddb8
Kedar Dhage <kedardhage0@gmail.com>
2025-10-02 19:26:56 +0530

    Set Interface in Java

---
+++ JavaCollections/src/MapInterface/MapMain.java
@@ -0,0 +1,7 @@
+package MapInterface;
+
+public class MapMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Map interface and its implemented class");
+    }
+}
