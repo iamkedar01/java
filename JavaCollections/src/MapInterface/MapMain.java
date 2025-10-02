0777c1cc76bfba2cbe5984835d0e5644d49138db
Kedar Dhage <kedardhage0@gmail.com>
2025-10-02 19:34:38 +0530

    Map Interface and Implemented Class

--- JavaCollections/src/MapInterface/MapMain.java
+++ JavaCollections/src/MapInterface/MapMain.java
@@ -2,6 +2,7 @@ package MapInterface;
 
 public class MapMain {
     public static void main(String[] args) {
+        // Here the details about the File
         System.out.println("Welcome to the Map interface and its implemented class");
     }
 }
