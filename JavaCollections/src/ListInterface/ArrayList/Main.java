de165816493ccd55083274d1fa6e04eea009f1c1
Kedar Dhage <kedardhage0@gmail.com>
2025-10-07 23:33:58 +0530

    ArrayList file Details

---
+++ JavaCollections/src/ListInterface/ArrayList/Main.java
@@ -0,0 +1,14 @@
+package ListInterface.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        /*  this file conatain the detains about the topic which we need to learn first
+            1)  ArrayList Declartion
+            2)  ArrayList Uses
+            3)  ArrayList Iterations
+            4)  ArrayList Sort
+
+         */
+        System.out.println("welcome to the ArrayList");
+    }
+}
