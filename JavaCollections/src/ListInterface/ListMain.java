8cae107272b1cfa5c7a0953dc39640a805ab2988
Kedar Dhage <kedardhage0@gmail.com>
2025-10-10 21:00:26 +0530

    file details of Listinterface classes

--- JavaCollections/src/ListInterface/ListMain.java
+++ JavaCollections/src/ListInterface/ListMain.java
@@ -4,5 +4,13 @@ public class ListMain {
     public static void main(String[] args) {
         // list in details
         System.out.println("Welcome to collection");
+        /*
+         1) add() ---> Adds an element to the end of the list
+         2) get()----> Returns the element at the specified position
+         3) set()--->Replaces the element at the specified position
+         4) remove()--->Removes the element at the specified position
+         5) size()--->Returns the number of elements in the list
+
+         */
     }
 }
