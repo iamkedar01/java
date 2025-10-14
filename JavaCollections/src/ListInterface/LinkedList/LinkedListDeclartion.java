e3d508bdc2d0bbccca6af90d2329900706028003
Kedar Dhage <kedardhage0@gmail.com>
2025-10-14 21:36:52 +0530

    LinkList Declartion

---
+++ JavaCollections/src/ListInterface/LinkedList/LinkedListDeclartion.java
@@ -0,0 +1,37 @@
+package ListInterface.LinkedList;
+import java.util.LinkedList;
+
+public class LinkedListDeclartion {
+    public static void main(String[] args) {
+        // To create an LinkedList Object first we need to import the LinkedList Package is a part of the
+        // java.util.*; package
+        /* Here the two way of the import
+        1)  we can import the only requried package only  like import java.util.LinkedList;
+        2)  we can import all package present in the java.util package  java.util.*;
+
+         */
+        // create an LinkedList object
+
+        LinkedList<String > Cars = new LinkedList <String>();
+        /* Here the explination  about the LinkedList object creation
+        1)  First Declration  the  object must be  LinkedList
+        2) Second Declare the Type of the data you want to store like String < >  , Integer , Double , Float , Character , Boolean etc ..
+        3) Third Assigen variable name that must be related to the data you want store
+        4) third use the new keyword to declare the object of the LinkedList with Variable name
+        5) fouth Again declare the LinkedList then type of the data inside the < > Condition statement
+        6) five to create an constructor use the  ()  double Bracket
+
+         */
+        //  Note: To declare the primitive data type we use the wrapper class
+        //  A Wrapper Class in Java is a class that is used to wrap (convert)
+        // a primitive data type (like int,long ,char, float , double , boolean , short , byte ) into an object.
+        // ( Integer ,Long, Character , Float , Double , Boolean , Short , Byte  )
+
+        /*
+        Wrapper class = a box for primitive data type
+            Boxing → putting primitive into a wrapper object.
+            Unboxing → taking primitive back out from the wrapper object.
+         */
+
+    }
+}
