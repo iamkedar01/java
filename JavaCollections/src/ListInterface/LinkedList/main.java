5daeebc371a5e29ea8e7e4a68f5a014f76e46505
Kedar Dhage <kedardhage0@gmail.com>
2025-10-28 22:52:10 +0530

    LinkedList File Details

---
+++ JavaCollections/src/ListInterface/LinkedList/main.java
@@ -0,0 +1,14 @@
+package ListInterface.LinkedList;
+
+public class main {
+    public static void main(String[] args) {
+        /*  this file conatain the detains about the topic which we need to learn first
+            1)  LinkedList Declartion
+            2)  LinkedList Uses
+            3)  LinkedList Iterations
+
+
+         */
+        System.out.println("welcome to the ArrayList in details and file Details ");
+    }
+}
