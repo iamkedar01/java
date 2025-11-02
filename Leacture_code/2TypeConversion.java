babd3ad995be1daa28488d684f92a303c0deb8c4
Kedar Dhage <kedardhage0@gmail.com>
2025-11-02 11:21:06 +0530

    Type converstion in Java

--- Leacture_code/2TypeConversion.java
+++ Leacture_code/2TypeConversion.java
@@ -3,7 +3,7 @@
         float myFloat = 5;
         System.out.println("\nBefore type converstion: "+ myFloat); 
         // here i am using the "\n" to move the newline
-        int myInt = (int) 5.45f; //float is covrting into a integer by explicit bcz float is grater then integer.
+        int myInt = (int) 5.45f; //float is convrting into a integer by explicit bcz float is grater then integer.
         System.out.println("After type converstion: "+ myInt); 
 
 
@@ -15,6 +15,11 @@
 
         float num1=num;
         System.out.println("After the type converstion: "+num1+"\n"); 
-        // this sprate "\n" new line will move the cursor to next line as showen in output
+        // this sperate "\n" new line will move the cursor to next line as showen in output
     }
 }
+/*
+        The mainly type conversion is two type
+        1)implicit type conversion ( converting small datatype into Bigger datatype )
+        2)Explicit Type conversion ( converting Bigger datatype into a smaller datatype)
+ */
