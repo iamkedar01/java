fe939f2e21ff5aad93bc9198e50552d71216a4b1
Kedar Dhage <kedardhage0@gmail.com>
2025-11-08 08:35:02 +0530

    Runtime Exception

---
+++ Exception_Handling/src/UncheckedException/RuntimeExceptionDemo.java
@@ -0,0 +1,33 @@
+package UncheckedException;
+
+
+//- The exceptions which occur during program execution are Runtime Exceptions
+//- All Runtime Exceptions are subclasses of RuntimeException class
+//- All Runtime Exceptions are categorized as unchecked exceptions
+
+public class RuntimeExceptionDemo { // This is also called the Unchecked Exception
+    public static void main(String[] args) {
+
+        // Note:  Every Excetion how occur it will explain
+        System.out.println("start of the program ");
+
+        // ArithmeticException
+//         int n =  10 /0 ;
+
+       // ArrayIndexOutOfBoundsException
+//        int[] arr = {1,2,3,4,5,6};
+//        System.out.println(arr[7]);
+
+      //  StringIndexOutOfBoundsException
+//        String name = "Kedar";
+//        System.out.println(name.charAt(7));
+
+      //  NegativeArraySizeException
+//         int[] arr = new int[-1];
+     //   NumberFormatException
+//        String s = "k";
+//        int n = Integer.parseInt(s);
+
+        System.out.println("End of the programm ");
+    }
+}
