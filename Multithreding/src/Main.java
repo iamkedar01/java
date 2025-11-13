30ab9be40111978cd42cef830f27f247e39f1657
Kedar Dhage <kedardhage0@gmail.com>
2025-11-13 18:17:26 +0530

    Multithreading in Java

---
+++ Multithreding/src/Main.java
@@ -0,0 +1,19 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        /*
+        it contian the file details
+        1)  Thread Class
+        2)  MultiThread Demo
+        3)  Runable Interface In Thread
+
+
+
+
+
+
+         */
+
+    }
+}
