ae92438609d67ddf274f2aa85ba611845fb18d61
Kedar Dhage <kedardhage0@gmail.com>
2025-11-17 17:27:25 +0530

    Exception Handleing File Details

--- Exception_Handling/src/Main.java
+++ Exception_Handling/src/Main.java
@@ -3,5 +3,16 @@
 public class Main {
     public static void main(String[] args) {
         // This Contain the file details i Exception Handling
+        /*
+         1) Exception Demo
+         2) Checked Exception
+         3) UnChecked Exception
+         4) Multiple Exception
+         5) ToString method
+         6) UserDefined Exception
+
+
+
+         */
     }
 }
