aaf8f142e090325275cc7c30373590709a96e65e
Kedar Dhage <kedardhage0@gmail.com>
2025-12-13 21:41:58 +0530

    updated

---
+++ Programming_Challenges/21PrimeNumber.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+ class PrimeNumber {
+     public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+         System.out.print("Enter a number: ");
+         int num = sc.nextInt();
+
+         if (num <= 1) {
+             System.out.println("Not a prime number");
+         } else {
+             boolean isPrime = true;
+
+             for (int i = 2; i <= Math.sqrt(num); i++) {
+                 if (num % i == 0) {
+                     isPrime = false;
+                     break;
+                 }
+             }
+
+             if (isPrime) {
+                 System.out.println("Prime number");
+             } else {
+                 System.out.println("Not a prime number");
+             }
+         }
+
+         sc.close();
+     }
+ }
+
