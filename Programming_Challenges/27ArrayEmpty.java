dd03e98005b1e0a7577a870cdf65d983564abd5f
Kedar Dhage <kedardhage0@gmail.com>
2025-12-19 20:15:18 +0530

    updated

---
+++ Programming_Challenges/27ArrayEmpty.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+ class CheckArrayEmpty {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        if (arr.length == 0) {
+            System.out.println("The array is empty.");
+        } else {
+            System.out.println("The array is not empty.");
+        }
+
+        sc.close();
+    }
+}
