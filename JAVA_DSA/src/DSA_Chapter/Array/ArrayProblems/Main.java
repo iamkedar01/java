cabf0ca97c29e7f389fcb80f0b27290ef62c0ba5
Kedar Dhage <kedardhage0@gmail.com>
2025-12-22 19:25:31 +0530

    Array Problem file Details

---
+++ JAVA_DSA/src/DSA_Chapter/Array/ArrayProblems/Main.java
@@ -0,0 +1,13 @@
+package DSA_Chapter.Array.ArrayProblems;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("File Details  of array");
+        /*
+        1)  Array Sum
+        2)  Arrya max
+        3)  Array min
+
+         */
+    }
+}
