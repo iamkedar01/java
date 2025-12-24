80b97a862630fb8f8996be61132cfe5239109fe4
Kedar Dhage <kedardhage0@gmail.com>
2025-12-24 21:30:01 +0530

    Mergeing Two Array

---
+++ Programming_Challenges/29MergeArray.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+class MergeArrays {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // First array
+        System.out.print("Enter size of first array: ");
+        int n1 = sc.nextInt();
+        int[] arr1 = new int[n1];
+        System.out.println("Enter elements of first array:");
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        // Second array
+        System.out.print("Enter size of second array: ");
+        int n2 = sc.nextInt();
+        int[] arr2 = new int[n2];
+        System.out.println("Enter elements of second array:");
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        // Merged array
+        int[] merged = new int[n1 + n2];
+
+        // Copy elements of first array
+        for (int i = 0; i < n1; i++) {
+            merged[i] = arr1[i];
+        }
+
+        // Copy elements of second array
+        for (int i = 0; i < n2; i++) {
+            merged[n1 + i] = arr2[i];
+        }
+
+        // Display merged array
+        System.out.println("Merged array:");
+        for (int i = 0; i < merged.length; i++) {
+            System.out.print(merged[i] + " ");
+        }
+
+        sc.close();  // this close the use of the Scanner function
+    }
+}
