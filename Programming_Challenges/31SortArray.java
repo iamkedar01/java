1dcabbdfd2500c5f7a7361a51601f97c919c0396
Kedar Dhage <kedardhage0@gmail.com>
2025-12-26 21:24:10 +0530

    Sort Array

---
+++ Programming_Challenges/31SortArray.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+ class SortArrayUsingArrays {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter array size: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        Arrays.sort(arr);   // sorting
+
+        System.out.println("Sorted array:");
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
