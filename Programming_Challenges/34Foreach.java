d052df374ccd868d7590c6672adf9808aaf49265
Kedar Dhage <kedardhage0@gmail.com>
2025-12-30 12:17:48 +0530

    For each loop in java

---
+++ Programming_Challenges/34Foreach.java
@@ -0,0 +1,12 @@
+class PrintArrayForEach {
+    public static void main(String[] args) {
+        // Declare and initialize array
+        int[] numbers = {10, 20, 30, 40, 50};
+        System.out.println("Array elements are:");
+
+        // Using for-each loop
+        for (int num : numbers) {
+            System.out.println(num);
+        }
+    }
+}
