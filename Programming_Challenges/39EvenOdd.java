22fcbd36aad2c5206ba805805884cc7f7d30b1ad
Kedar Dhage <kedardhage0@gmail.com>
2026-01-10 18:13:05 +0530

    Even and Odd Number

---
+++ Programming_Challenges/39EvenOdd.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+ class EvenOdd {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+        int n = sc.nextInt();
+
+        if (n % 2 == 0)
+            System.out.println("Even");
+        else
+            System.out.println("Odd");
+    }
+}
