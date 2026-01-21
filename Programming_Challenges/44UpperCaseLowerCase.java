58d907c8ab78b6b29d972e629a2d7d549550158d
Kedar Dhage <kedardhage0@gmail.com>
2026-01-21 12:52:10 +0530

    Upper Case to Lower Case

---
+++ Programming_Challenges/44UpperCaseLowerCase.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class CaseCheck {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a any character:");
+        char ch = sc.next().charAt(0);
+
+        if (ch >= 'A' && ch <= 'Z')
+            System.out.println("Uppercase");
+        else if (ch >= 'a' && ch <= 'z')
+            System.out.println("Lowercase");
+        else
+            System.out.println("Not an alphabet");
+    }
+}
